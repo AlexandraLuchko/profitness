4298f374371bdec322bb55035c8afd09bb4fe68f
Aliaksandra Luchko <lu4ko.aleksandra@gmail.com>
2021-10-28 19:31:35 +0300

    Created project and added mui openup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Popover from '@mui/material/Popover';
+import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function PopoverPopupState() {
+
+  const bodyParts = [
+    {name: 'Arms', muscles: ['Triceps', 'biceps', 'underarm']},
+    {name: 'Feet', muscles: ['Quadrum', 'knee-joint', 'ankle']},
+    {name: 'Corpus', muscles: ['abdominal muscles', 'lower abdominals', 'obliques']},
+    {name: 'Back', muscles: ['latissimus', 'rhomboid muscle', 'trapezius', 'psoas muscle', 'extensor muscle']},
+    {name: 'Buttocks', muscles: ['glute max', '	mesogluteus', 'gluteus minimus muscle']},
+];
+
+  return (   
+    <PopupState variant="popover" popupId="demo-popup-popover">    
+      {(popupState) => (        
+        <div>
+        {bodyParts.map((bodyPart) => (
+          <div>
+          <Button variant="contained" {...bindTrigger(popupState)}>
+              {bodyPart.name}
+          </Button>
+          <Popover
+            {...bindPopover(popupState)}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+          >
+          {bodyPart.muscles.map((muscle) => (
+            <Typography sx={{ p: 2 }}>
+            {muscle + ' '}
+            </Typography>
+            ))}
+          </Popover>
+          </div>
+          ))}
+          
+        </div>
+      )}     
+    </PopupState>
   );
 }
-
-export default App;
